a8ce16cd87d7924ec977a3a0fc1979fc2a96360f
Thomas Kane <thomasjohnkane@gmail.com>
2019-07-10 21:24:50 -0500

    minor clean up

--- database/migrations/2019_07_10_175701_create_users_table.php
+++ database/migrations/2019_07_10_175701_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('api_token');
+            $table->string('api_token', 80)->unique()->nullable()->default(null);;
             $table->string('remember_token')->nullable();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
